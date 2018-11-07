4b7ced1dbf0acb12a6e2911322119a9b4ab9c272
keops3312 <keops_3312@hotmail.com>
2018-11-07 14:13:24 -0600

    RESOURCES
    RESOURCES

---
+++ EKsemp/Views/EncryptDecryptForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace EKsemp.Views
+{
+    partial class EncryptDecryptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSelect = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.DisplayMember = "Text";
+            this.cmbSelect.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.ItemHeight = 14;
+            this.cmbSelect.Location = new System.Drawing.Point(288, 160);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(371, 20);
+            this.cmbSelect.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cmbSelect.TabIndex = 1;
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(361, 120);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(75, 23);
+            this.labelX1.TabIndex = 2;
+            this.labelX1.Text = "labelX1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EKsemp.Properties.Resources.Header_Encryption;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EncryptDecryptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.cmbSelect);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "EncryptDecryptForm";
+            this.Text = "EncryptDecryptForm";
+            this.Load += new System.EventHandler(this.EncryptDecryptForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cmbSelect;
+        private DevComponents.DotNetBar.LabelX labelX1;
+    }
+}
---
+++ EKsemp/Views/EncryptDecryptForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EKsemp.Views
+{
+    public partial class EncryptDecryptForm : Form
+    {
+        public EncryptDecryptForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public void LoadF()
+        {
+            cmbSelect.Items.Add("MD5");
+            cmbSelect.Items.Add("StringSapher");
+            cmbSelect.Items.Add("RSA Encrytpion");
+            cmbSelect.Items.Add("Tripple");
+            
+        }
+
+
+        private void EncryptDecryptForm_Load(object sender, EventArgs e)
+        {
+            LoadF();
+        }
+    }
+}

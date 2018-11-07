2e38ce29e5ca3a1695e2e857c3638d429762b969
keops3312 <keops_3312@hotmail.com>
2018-11-07 12:17:58 -0600

    Crypto Methods
    Creating crypto methods

---
+++ EDsemp/Classes/Encriptar_Desencriptar.cs
@@ -0,0 +1,281 @@
+﻿
+
+namespace EDsemp.Classes
+{
+    #region Libraries(Libreriras)
+    using System;
+    using System.Collections;
+    using System.IO;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    #endregion
+
+    public class Encriptar_Desencriptar
+    {
+
+        #region attributes (atributos)
+        string key = "ABCDEFG54669525PQRSTUVWXYZabcdef852846opqrstuvwxyz";
+        #endregion
+
+        #region Methods (Metodos)
+
+
+        #region MD5
+        //ENCRIPTA CONTRASEÑA
+        public string EncryptKeyMD5(string cadena)
+        {
+            //arreglo de bytes donde guardaremos la llave
+
+            byte[] keyArray;
+
+            //arreglo de bytes donde guardaremos el texto
+
+            //que vamos a encriptar
+
+            byte[] Arreglo_a_Cifrar = UTF8Encoding.UTF8.GetBytes(cadena);
+
+            //se utilizan las clases de encriptación
+
+            //provistas por el Framework
+
+            //Algoritmo MD5
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            //se guarda la llave para que se le realice
+
+            //hashing
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+
+            hashmd5.Clear();
+
+            //Algoritmo 3DAS
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+            tdes.Key = keyArray;
+
+            tdes.Mode = CipherMode.ECB;
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            //se empieza con la transformación de la cadena
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+
+            //arreglo de bytes donde se guarda la
+
+            //cadena cifrada
+
+            byte[] ArrayResultado = cTransform.TransformFinalBlock(Arreglo_a_Cifrar, 0, Arreglo_a_Cifrar.Length);
+
+            tdes.Clear();
+
+            //se regresa el resultado en forma de una cadena
+
+            return Convert.ToBase64String(ArrayResultado, 0, ArrayResultado.Length);
+        }
+        //DESENCRIPTA CONTRASEÑA
+        public string DecryptKeyMD5(string clave)
+        {
+            byte[] keyArray;
+
+            //convierte el texto en una secuencia de bytes
+
+            byte[] Array_a_Descifrar = Convert.FromBase64String(clave);
+
+            //se llama a las clases que tienen los algoritmos
+
+            //de encriptación se le aplica hashing
+
+            //algoritmo MD5
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+            tdes.Key = keyArray;
+
+            tdes.Mode = CipherMode.ECB;
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+
+            byte[] resultArray = cTransform.TransformFinalBlock(Array_a_Descifrar, 0, Array_a_Descifrar.Length);
+
+            tdes.Clear();
+
+            //se regresa en forma de cadena
+
+            return UTF8Encoding.UTF8.GetString(resultArray);
+
+        }
+        #endregion
+
+        #region  StringSapher
+        // This constant is used to determine the keysize of the encryption algorithm in bits.
+        // We divide this by 8 within the code below to get the equivalent number of bytes.
+        private const int Keysize = 256;
+
+        // This constant determines the number of iterations for the password bytes generation function.
+        private const int DerivationIterations = 1000;
+
+        public static string EncryptStringSapher(string plainText, string passPhrase)
+        {
+            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+            // so that the same Salt and IV values can be used when decrypting.  
+            var saltStringBytes = Generate256BitsOfRandomEntropy();
+            var ivStringBytes = Generate256BitsOfRandomEntropy();
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            {
+                var keyBytes = password.GetBytes(Keysize / 8);
+                using (var symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.BlockSize = 256;
+                    symmetricKey.Mode = CipherMode.CBC;
+                    symmetricKey.Padding = PaddingMode.PKCS7;
+                    using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                cryptoStream.FlushFinalBlock();
+                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                                var cipherTextBytes = saltStringBytes;
+                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                memoryStream.Close();
+                                cryptoStream.Close();
+                                return Convert.ToBase64String(cipherTextBytes);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string DecryptSapher(string cipherText, string passPhrase)
+        {
+            // Get the complete stream of bytes that represent:
+            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
+            var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
+            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
+            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+
+            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            {
+                var keyBytes = password.GetBytes(Keysize / 8);
+                using (var symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.BlockSize = 256;
+                    symmetricKey.Mode = CipherMode.CBC;
+                    symmetricKey.Padding = PaddingMode.PKCS7;
+                    using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                    {
+                        using (var memoryStream = new MemoryStream(cipherTextBytes))
+                        {
+                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                            {
+                                var plainTextBytes = new byte[cipherTextBytes.Length];
+                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                memoryStream.Close();
+                                cryptoStream.Close();
+                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static byte[] Generate256BitsOfRandomEntropy()
+        {
+            var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with cryptographically secure random bytes.
+                rngCsp.GetBytes(randomBytes);
+            }
+            return randomBytes;
+        }
+        #endregion
+
+
+        #region RSA Encrytpion
+
+        public string EncryptStringRSA(string inputString, int dwKeySize,
+                                string xmlString)
+        {
+            RSACryptoServiceProvider rsaCryptoServiceProvider =
+                                      new RSACryptoServiceProvider(dwKeySize);
+            rsaCryptoServiceProvider.FromXmlString(xmlString);
+            int keySize = dwKeySize / 8;
+            byte[] bytes = Encoding.UTF32.GetBytes(inputString);
+            //RSACryptoServiceProvider here
+            int maxLength = keySize - 42;
+            int dataLength = bytes.Length;
+            int iterations = dataLength / maxLength;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i <= iterations; i++)
+            {
+                byte[] tempBytes = new byte[
+                        (dataLength - maxLength * i > maxLength) ? maxLength :
+                                                      dataLength - maxLength * i];
+                Buffer.BlockCopy(bytes, maxLength * i, tempBytes, 0,
+                                  tempBytes.Length);
+                byte[] encryptedBytes = rsaCryptoServiceProvider.Encrypt(tempBytes,
+                                                                          true);
+                stringBuilder.Append(Convert.ToBase64String(encryptedBytes));
+            }
+            return stringBuilder.ToString();
+        }
+        public string DecryptStringRSA(string inputString, int dwKeySize, string xmlString)
+        {
+            RSACryptoServiceProvider rsaCryptoServiceProvider
+                                         = new RSACryptoServiceProvider(dwKeySize);
+            rsaCryptoServiceProvider.FromXmlString(xmlString);
+            int base64BlockSize = ((dwKeySize / 8) % 3 != 0) ?
+              (((dwKeySize / 8) / 3) * 4) + 4 : ((dwKeySize / 8) / 3) * 4;
+            int iterations = inputString.Length / base64BlockSize;
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < iterations; i++)
+            {
+                byte[] encryptedBytes = Convert.FromBase64String(
+                     inputString.Substring(base64BlockSize * i, base64BlockSize));
+                Array.Reverse(encryptedBytes);
+                arrayList.AddRange(rsaCryptoServiceProvider.Decrypt(
+                                    encryptedBytes, true));
+            }
+            return Encoding.UTF32.GetString(arrayList.ToArray(
+                                      Type.GetType("System.Byte")) as byte[]);
+        }
+
+
+        #endregion
+
+
+        #region Tripple
+
+        #endregion
+
+
+        //Methods III Triple Encrypt
+       
+        #endregion
+
+
+
+    }
+}
